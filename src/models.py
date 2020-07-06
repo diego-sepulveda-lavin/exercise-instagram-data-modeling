a9d58174e6ec9de0c3d5d7cd2e2c07c9422972f4
diego-sepulveda-lavin <diegosl@hotmail.cl>
2020-07-06 17:51:42 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,76 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(20), nullable=False)
+    firstname = Column(String(60), nullable=False)
+    lastname = Column(String(60), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    posts = relationship('Post', backref='author', lazy=True)
+    followers = relationship('Follower', backref='followers', lazy=True)
+    following = relationship('Follower', backref='following', lazy=True)
+
 
     def to_dict(self):
-        return {}
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.emal
+        }
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    comments = relationship('Comment', backref='post', lazy=True)
+    medias = relationship('Media', backref='post', lazy=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id
+        }
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(255), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "comment_text": self.comment_text,
+            "author_id": self.author_id,
+            "post_id": self.post_id
+        }
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    kind = Column(String, nullable=False)
+    url = Column(String, nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "kind": self.kind,
+            "url": self.url,
+            "post_id": self.post_id
+        }
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
