f778454739971f1745ef125389c559e1acaee6ec
diego-sepulveda-lavin <diegosl@hotmail.cl>
2021-10-11 22:38:36 +0000

    fix

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+import enum
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,75 +9,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=True)
-    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=True)
-
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    username = Column(String(20), nullable=False)
-    firstname = Column(String(60), nullable=False)
-    lastname = Column(String(60), nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
-    posts = relationship('Post', backref='author', lazy=True)
-    followers = relationship('Follower', backref='followers', lazy=True)
-    following = relationship('Follower', backref='following', lazy=True)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    posts = relationship('Post', backref='user', lazy=True)
+    comments = relationship('Comment', backref='user', lazy=True)
+    followers = relationship('Follower', backref='user', lazy=True)
+    followed = relationship('Follower', backref='user', lazy=True)
 
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "username": self.username,
-            "firstname": self.firstname,
-            "lastname": self.lastname,
-            "email": self.emal
-        }
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user_to_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    comments = relationship('Comment', backref='post', lazy=True)
     medias = relationship('Media', backref='post', lazy=True)
+    comments = relationship('Comment', backref='post', lazy=True)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id
-        }
-
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
-    comment_text = Column(String(255), nullable=False)
-    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "comment_text": self.comment_text,
-            "author_id": self.author_id,
-            "post_id": self.post_id
-        }
+class MyEnum(enum.Enum):
+    photo = 1
+    video = 2
+    gif = 3
 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    kind = Column(String, nullable=False)
-    url = Column(String, nullable=False)
+    type = Column(Enum(MyEnum), nullable=False)
+    url = Column(String(250), nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "kind": self.kind,
-            "url": self.url,
-            "post_id": self.post_id
-        }
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    author_id = Column(Integer,ForeignKey('user.id'), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
 
 
 ## Draw from SQLAlchemy base
